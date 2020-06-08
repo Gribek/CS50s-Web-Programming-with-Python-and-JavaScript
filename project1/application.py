34f0040e6be8c5deaa934f9e1847f721d7c408a1
Gribek <piotrekmaslewski@gmail.com>
2020-06-08 13:36:33 +0200

    Added file to import csv data to postgre db.

--- project1/application.py
+++ project1/application.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect, url_for, \
-    flash
+from flask import Flask, session, render_template, request, redirect, \
+    url_for, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
