bc69c0c477e69f3a002a830edd4c0baaccc1f90e
Gribek <piotrekmaslewski@gmail.com>
2020-06-16 11:34:58 +0200

    Created Channel and Message classes.

--- project2/application.py
+++ project2/application.py
@@ -7,6 +7,43 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Maximum number of messages stored per channel
+CHANNEL_MAX_MESSAGES = 100
+
+
+class Channel:
+    """Represents a single channel"""
+
+    def __init__(self, name):
+        self.__name = name
+        self.__storage_full = False
+        self.messages = []
+
+    def __str__(self):
+        return self.__name
+
+    def add_message(self, author, text):
+        """Save new message"""
+
+        # Create and save new message
+        m = Message(author, text)
+        self.messages.append(m)
+
+        # Check if the messages limit has been reached
+        if self.__storage_full:
+            del self.messages[0]
+        else:
+            if len(self.messages) == CHANNEL_MAX_MESSAGES:
+                self.__storage_full = True
+
+
+class Message:
+    """Represents a single message"""
+
+    def __init__(self, author, text):
+        self.author = author
+        self.text = text
+
 
 @app.route("/")
 def index():
