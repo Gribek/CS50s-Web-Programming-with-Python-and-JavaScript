b365d65a4cdecd1b83bb0b2367d9819da6279062
Gribek <piotrekmaslewski@gmail.com>
2020-06-17 13:26:40 +0200

    Added comments

--- project2/static/app.js
+++ project2/static/app.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Get two html sections
     let intro_section = document.querySelector('#introduction')
     let channels_section = document.querySelector('#channels')
 
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // Check if user name is in local storage, hide channels section if not
     if (!localStorage.getItem('name')) {
         intro_section.hidden = false;
         channels_section.hidden = true;
 
+        // When user submit name save it to the local storage and display channels section
         document.querySelector('#form').onsubmit = () => {
             let name = intro_section.querySelector('input').value
             localStorage.setItem('name', name);
