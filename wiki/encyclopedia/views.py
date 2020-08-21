4595af8ac229a812b2fc07ba39eb2fa59befe964
Gribek <piotrmaslewski@gmail.com>
2020-08-21 12:40:48 +0200

    Add wiki entry page
    Entry page displays single wiki entry, using entry_page view function, entry_page.html template and entry_page path.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +10,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry_page(request, entry_title):
+    entry = util.get_entry(entry_title)
+    if entry is None:
+        raise Http404
+    mark_downer = Markdown()
+    ctx = {'content': mark_downer.convert(entry), 'title': entry_title}
+    return render(request, 'encyclopedia/entry_page.html', context=ctx)
