b815d41ab3e611acdfc881f987d1ca99b3685f48
Gribek <piotrmaslewski@gmail.com>
2020-10-26 14:15:05 +0100

    Add new model representing posts

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    text = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
